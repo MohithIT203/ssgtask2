d4fdafb1d14d1ab6bbd55428859241a55f5789e5
HARJIT M <harjitrm@gmail.com>
2024-11-06 08:54:51 +0530

    newly car and bike committed

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,16 @@
-import CarOptions from './car2.jsx';
+import CarOptions from './car1-2.jsx';
 import BikeDropdowns from './bike1.jsx';
 import BikeOptions from './bike2.jsx';
 import Header from './header.jsx';
-import CarDropdowns from './car1.jsx';
 import Food from './faq.jsx';
-import Bikepage3 from './bike3.jsx';
+import Car1Dropdowns from './car1-1.jsx';
+import Car1Options from './car1-2.jsx';
 // import Bikeinspic from './bikepic.jsx';
 function App() {
     return (
         <>
         <Header></Header>
-        <BikeDropdowns></BikeDropdowns>
+        <Car1Options></Car1Options>
        </>
     );
 }
